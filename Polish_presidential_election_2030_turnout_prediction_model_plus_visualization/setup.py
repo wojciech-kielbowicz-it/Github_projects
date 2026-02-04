d9b9b3a31e68a1c69c39d7dcac9139f76e5182ac
Wojciech Kiełbowicz <wojciech_kielbowicz_it@gmail.com>
2026-02-04 14:20:42 +0100

    Project readme...

---
+++ Polish_presidential_election_2030_turnout_prediction_model_plus_visualization/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Polish presidential_election_turnout_prediction_model_plus_visualization',
+    author='Wojciech Kiełbowicz',
+    license='MIT',
+)
