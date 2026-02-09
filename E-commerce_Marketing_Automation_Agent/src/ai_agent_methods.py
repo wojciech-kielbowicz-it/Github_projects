60d336101987d1b36b7cef61274cb50ecfb68117
Wojciech Kiełbowicz <wojciech_kielbowicz_it@gmail.com>
2026-02-09 18:57:20 +0100

    add ai_agent_methods / add generate_email method

---
+++ E-commerce_Marketing_Automation_Agent/src/ai_agent_methods.py
@@ -0,0 +1,27 @@
+from huggingface_hub import InferenceClient
+import pandas as pd
+import time
+
+def generate_email(row: pd.Series, prompt: dict) -> str:
+    segment = row["segmentation"]
+    instruction = prompt[segment]
+    try:
+        completion = client.chat.completions.create(
+            model="HuggingFaceH4/zephyr-7b-beta:featherless-ai",
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"{instruction} Customer ID: {row['customer_id']}"
+                }
+            ],
+            max_tokens=150, 
+            temperature=0.7
+
+        )
+        message = completion.choices[0].message.content
+        time.sleep(1)
+        return message
+    except Exception as e:
+        print(f"Error:\n{e}")
+        return "ERROR"
+        
