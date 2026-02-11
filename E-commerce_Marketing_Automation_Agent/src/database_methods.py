76646e4f06f8619f41c9956803869008860ab21d
Wojciech Kiełbowicz <wojciech_kielbowicz_it@gmail.com>
2026-02-11 13:47:37 +0100

    fix typing database_methods

--- E-commerce_Marketing_Automation_Agent/src/database_methods.py
+++ E-commerce_Marketing_Automation_Agent/src/database_methods.py
@@ -2,11 +2,11 @@ from collections.abc import Iterable
 import csv
 from io import StringIO
 import os
-from sqlalchemy import create_engine, Connection
+from sqlalchemy import create_engine, Connection, Engine, Table
 from dotenv import load_dotenv
 
 
-def get_db_engine():
+def get_db_engine() -> Engine:
     
     load_dotenv()
     user = os.getenv("DB_USER")
@@ -15,17 +15,17 @@ def get_db_engine():
     port = os.getenv("DB_PORT")
     db_name = os.getenv("DB_NAME")
 
-    connection_string = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}"
+    connection_string: str= f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}"
 
-    engine = create_engine(connection_string)
+    engine: Engine = create_engine(connection_string)
 
     return engine
 
-def psql_insert_copy[T: {any}](
-        table: T, 
+def psql_insert_copy(
+        table: Table, 
         connect: Connection, 
         keys: list[str], 
-        data_iter: Iterable[tuple[T,...]]
+        data_iter: Iterable[tuple[object,...]]
 ) -> int|None :
 
     s_buf = StringIO()
@@ -35,6 +35,7 @@ def psql_insert_copy[T: {any}](
 
     dbapi_conn = connect.connection
     with dbapi_conn.cursor() as cur:
-        schema_prefix = f"{table.schema}." if table.schema else ""
-        sql = f"COPY {schema_prefix}{table.name} ({', '.join(keys)}) FROM STDIN WITH CSV"
+        schema_prefix: str = f"{table.schema}." if table.schema else ""
+        sql: str = f"COPY {schema_prefix}{table.name} ({', '.join(keys)}) FROM STDIN WITH CSV"
         cur.copy_expert(sql=sql, file=s_buf)
+        return cur.rowcount
